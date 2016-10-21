9a29a4d841a30297a4b49e2889d016263aa818c5
terra <terra>
2016-10-21 18:52:42 -0200

    commit

---
+++ footer.php
@@ -0,0 +1,13 @@
+                <div class="footer clearfix">
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1">
+                            <div class="row">
+                                <div class="col-md-6 col-sm-12 col-xs-12">
+                                    <p class="copyright">Copyright © 2016 
+                                        <a href="www.matheuspuppe.com.br">Matheus Nunes Puppe</a>
+                                    </p>
+                                </div>
+                            </div>      
+                        </div>
+                    </div>
+                </div>
